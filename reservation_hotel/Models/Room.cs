54bbde15b485fdada6a2bf39c9081c278b1fd772
Fernando Henrique Lotti <fernand0devel0per@hotmail.com>
2022-09-21 10:26:44 -0300

    Change models to records

--- reservation_hotel/Models/Room.cs
+++ reservation_hotel/Models/Room.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace reservation_hotel.Models
+﻿namespace reservation_hotel.Models
 {
-    public class Room
-    {
-        public int Number { get; set; }
-        public int Space { get; set; }
-        public int Category { get; set; }
-        public bool IsFree { get; set; }
-    }
+    public record Room(int Number, int Space, int Category, bool IsFree);
 }
--- reservation_hotel/Models/User.cs
+++ reservation_hotel/Models/User.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace reservation_hotel.Models
+﻿namespace reservation_hotel.Models
 {
-    public class User
-    {
-        public string Name { get; set; }
-        public string Cpf { get; set; }
-        public string Phone { get; set; }
-    }
+    public record User(string Name, string Cpf, string Phone);
 }
